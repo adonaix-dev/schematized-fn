b1dbd548758862bf56450a1034125e6e3a65f685
Arthur Adonai <tutuadonai@gmail.com>
2026-02-17 23:03:05 -0300

    feat: expose public api modules

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export { SchematizedArgs } from "~/Core/SchematizedArgs";
+export { SchematizedFunction } from "~/Core/SchematizedFunction";
+export { SchematizedOverloads } from "~/Core/SchematizedOverloads";
+
+export { SchematizedError } from "~/Error/SchematizedError";
+export { ArgumentsError } from "~/Error/ArgumentsError";
+export { NoMatchingOverloadError } from "~/Error/NoMatchingOverloadError";
+export { SynchronousValidationError } from "~/Error/SynchronousValidationError";
+
+export type { StdSchemaV1 } from "~/Common/StdSchemaV1";
+export type { SchematizedArgsDefinition } from "~/Types/Definition/SchematizedArgsDefinition";
+
+export type { SchematizedImplementation } from "~/Types/Function/SchematizedImplementation";
+export type { SchematizedOutputArgs } from "~/Types/Function/SchematizedOutputArgs";
+export type { SchematizedInputArgs } from "~/Types/Function/SchematizedInputArgs";
+
+export type { SchematizedOverloadSignature } from "~/Types/Overload/SchematizedOverloadSignature";
+export type { SchematizedOverloadedArgs } from "~/Types/Overload/Internal/SchematizedOverloadedArgs";
+export type { SchematizedOverloadedReturn } from "~/Types/Overload/Internal/SchematizedOverloadedReturn";
+export type { SchematizedOverloadedFunction } from "~/Types/Overload/SchematizedOverloadedFunction";
