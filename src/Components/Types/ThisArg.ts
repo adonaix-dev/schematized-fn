849454d7abcdc56f8ff2e88f75ba56d325a25e4c
Arthur Adonai <tutuadonai@gmail.com>
2026-02-18 01:07:38 -0300

    refactor: update ThisArg type to handle void and undefined instead of missing type

--- src/Components/Types/ThisArg.ts
+++ src/Components/Types/ThisArg.ts
@@ -1,5 +1,7 @@
-import type { missing } from "@adonaix/types";
-
-type ThisArg<Type> = unknown extends Type ? void : Type extends missing ? void : Type;
+type ThisArg<Type> = unknown extends Type
+    ? void
+    : Type extends void | undefined
+      ? void
+      : Type;
 
 export type { ThisArg };
